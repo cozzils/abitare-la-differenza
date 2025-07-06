f621f82270b042567c9c7de3d0c1dbdca5814e8f
Luca Cozzi <cozziluca1@gmail.com>
2025-07-06 22:25:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/abitare-la-differenza/', // usa il nome repo corretto qui
-})
+  base: '/abitare-la-differenza/', // 👈 nome esatto del repo
+});
