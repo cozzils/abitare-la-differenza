70d03c96afed58b0eb0f322a16a3f3c79bc1207a
Luca Cozzi <cozziluca1@gmail.com>
2025-07-06 20:19:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/abitare-la-differenza/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
+    rollupOptions: {
+      input: 'index.html',
+    },
   },
-});
+})
+
