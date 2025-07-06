80029ee4c216d8443be903ecb2a19120462a99bb
Luca Cozzi <cozziluca1@gmail.com>
2025-07-06 22:42:20 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/abitare-la-differenza/', // 👈 nome esatto del repo
-});
