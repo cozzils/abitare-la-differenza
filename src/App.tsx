b724d31546afbb91714c28f7bd778df26e324985
Luca Cozzi <cozziluca1@gmail.com>
2025-07-06 20:02:56 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Abitare la Differenza</h1>
+        <p>Evento Scout BS11</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
